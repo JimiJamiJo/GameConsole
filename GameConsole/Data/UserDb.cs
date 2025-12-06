98816646d3b14f69bfb24149c52f56fd6d8f69db
Jonathan <jonathanedri13@gmail.com>
2025-12-06 14:01:44 +0200

    Add files via upload

---
+++ GameConsole/Data/UserDb.cs
@@ -0,0 +1,50 @@
+﻿using GameConsole.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameConsole.Data
+{
+    internal class UserDb
+    {
+        private static List<User> users;
+
+
+
+        public static User RegisterUser(string name, string userName, string password)
+        {
+            User newUser = new User(name, userName, password);
+            users.Add(newUser);
+
+            return newUser;
+        }
+
+        public static User Login(string username, string password)
+        {
+            foreach (var user in users)
+            {
+                if (user.UserName == username && user.Password == password) { return user;  }
+            }
+
+            return null;
+        }
+
+        public static void Update(User u)
+        {
+            foreach (var user in users)
+            {
+                if (user.UserName == u.UserName) 
+                { 
+                    u.UserName = user.UserName; 
+                    u.Password = user.Password;
+                    return;
+                }
+            }
+
+            throw new InvalidOperationException("no such user exists!");
+        }
+    }
+}
